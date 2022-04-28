87b6d7c8c17c4841a66f505528f3f45ea3fb6f74
Seoyeon Yoon <s2013@e-mirim.hs.kr>
2022-04-28 10:05:45 +0900

    UserManageApp 컴포넌트 만들기

--- src/use-callback-memo-demo/user-manage-app.js
+++ src/use-callback-memo-demo/user-manage-app.js
@@ -13,18 +13,31 @@ const UserManageApp = () => {
     const [age, setAge] = useState(20)
 
     // useCallback 사용하여 새 사용자 추가하는 함수 생성 및 저장
-    const addUser = null
+    const addUser = () => setUsers(users.concat({name, age}))
+
     // useMemo 사용하여 나이 20 미만인 미성년자 수 저장
-    const minorCount = null
+    //반복문
+    const minorCount = (() => {
+        let count = 0;
+        for(const user of users) {
+            if(user.age < 20) count++;
+        }
+
+        return count
+    })()
+
     // useMemo 사용하여 나이 20 미만인 미성년자의 이름을 배열 형태로 저장
-    const minorNames = null
+    //filter
+    const minorNames = (() => {
+        return users.filter(user => user.age < 20);
+    })();
 
     return (
         <div>
             <div>
-                <label>이름</label> <input type="text" /><br />
-                <label>나이</label> <input type="number" /><br />
-                <button>회원 추가</button>
+                <label>이름</label> <input type="text" vlaue = {name} onChange = {e => setName(e.target.value)}/><br />
+                <label>나이</label> <input type="number" vlaue = {age} onChange = {e => setAge(e.target.value)}/><br />
+                <button onClick = {addUser}> 회원 추가</button>
             </div>
             <hr />
             <div>미성년자 회원 수 : {minorCount}</div>
