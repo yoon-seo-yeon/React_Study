875f5a3fa5e0d12ecb296a329d11d31dd2ab134d
Seoyeon Yoon <s2013@e-mirim.hs.kr>
2022-05-26 08:49:22 +0900

    모달창과 로딩스크린

--- src/index.js
+++ src/index.js
@@ -39,4 +39,4 @@
 // import './use-callback-memo-demo/user-manage-app.js';
 // import './use-callback-memo-demo/using-react-memo.js';
 //import './use-callback-memo-demo/drawstar.js';
-import './custom-hook-demo/use-async-demo.js';
+import './etc/using-portal.js';
